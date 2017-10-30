b923fc59948888eafbcd66f5369a0be1e026d217
Pavel Krasnov <krasnovpavel0@gmail.com>
2017-10-30 22:00:16 +0300

    Initial for Core module

---
+++ DSD_Core/DSDObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pavel on 10/30/17.
+//
+
+#include "DSDObject.h"
---
+++ DSD_Core/DSDObject.h
@@ -0,0 +1,17 @@
+//
+// Created by pavel on 10/30/17.
+//
+
+#ifndef DSD_GAMEENGINE_DSDOBJECT_H
+#define DSD_GAMEENGINE_DSDOBJECT_H
+
+#include <string>
+
+class DSDObject
+{
+public:
+    virtual std::string ToString() const = 0;
+};
+
+
+#endif //DSD_GAMEENGINE_DSDOBJECT_H
