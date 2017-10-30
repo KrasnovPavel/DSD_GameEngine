2f70875c4f169b27d9195350c55dbb24b73e9b87
Pavel Krasnov <krasnovpavel0@gmail.com>
2017-10-30 22:00:16 +0300

    Initial for Core module

---
+++ DSD_Core/DSDObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pavel Krasnov (krasnovpavel0@gmail.com) on 10/30/17.
+//
+
+#include "DSDObject.h"
--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
+#include "DSD_Core/DSDObject.h"
+
+class Obj : public DSDObject
+{
+public:
+    std::string ToString() const override
+    {
+        return "Hello, world!";
+    }
+};
+
 int main()
 {
+    Obj o;
+
+    std::cout << o.ToString() << std::endl;
 
     return 0;
 }
