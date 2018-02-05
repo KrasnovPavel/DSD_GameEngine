6321a26943228d8becbcb5fb092ee6d5369b5a94
Pavel Krasnov <krasnovpavel0@gmail.com>
2018-02-05 16:15:49 +0300

    Documentation for Vector3 class was added.

--- Core/Math/Vector3.h
+++ Core/Math/Vector3.h
@@ -9,18 +9,48 @@
 
 #include "Core/DSDBaseObject.h"
 
+/**
+ * @defgroup Math
+ * @brief Mathematics implementation
+ * @{
+ */
+
+/**
+ * @brief Implements vector and operations with it in three-dimensional space.
+ */
 class Vector3 : public DSDBaseObject
 {
-    REFLECTION(Vector3);
+REFLECTION(Vector3);
 public:
-    Vector3() {}
+    /**
+     * @brief Default constructor
+     * @details Fills values with zeroes.
+     */
+    Vector3()
+            : x(0), y(0), z(0)
+    {}
 
+    /**
+     * @brief Default copy-constructor
+     * @param other
+     */
     Vector3(Vector3&& other) noexcept
-        : x(other.x), y(other.y), z(other.z), DSDBaseObject(other) {}
+            : x(other.x), y(other.y), z(other.z), DSDBaseObject(other) {}
 
+    /**
+     * @brief Default move-constructor
+     * @param other
+     */
     Vector3(const Vector3& other)
-        : x(other.x), y(other.y), z(other.z), DSDBaseObject(other) {}
+            : x(other.x), y(other.y), z(other.z), DSDBaseObject(other) {}
 
+    /**
+     * @brief Constructor
+     * @details Constructs new object and sets values.
+     * @param x 1-st coordinate
+     * @param y 2-nd coordinate
+     * @param z 3-rd coordinate
+     */
     inline Vector3(const double& x, const double& y, const double& z)
     {
         this->x = x;
@@ -28,6 +58,12 @@ public:
         this->z = z;
     }
 
+    /**
+     * @brief Sets coordinates
+     * @param x 1-st coordinate
+     * @param y 2-nd coordinate
+     * @param z 3-rd coordinate
+     */
     void set(const double& x, const double& y, const double& z)
     {
         this->x = x;
@@ -35,35 +71,76 @@ public:
         this->z = z;
     }
 
+    /**
+     * @brief Calculates cross product of two vectors
+     * @param first
+     * @param second
+     * @return first\f$\bullet\f$second
+     */
     static double dot(const Vector3& first, const Vector3& second);
 
+    /**
+     * @brief Calculates dot product of this and other given vectors
+     * @param other
+     * @return first\f$\bullet\f$other
+     */
     inline double dot(const Vector3 &other) const
     {
         return Vector3::dot(*this, other);
     }
 
+    /**
+     * @brief Calculates cross product of two vectors
+     * @param first
+     * @param second
+     * @return first\f$\times\f$second
+     */
     static Vector3 cross(const Vector3& first, const Vector3& second);
 
+    /**
+     * @brief Calculates cross product of this and other given vectors
+     * @param other
+     * @return first\f$\times\f$other
+     */
     Vector3 cross(const Vector3& other) const
     {
         return Vector3::cross(*this, other);
     }
 
+    /**
+     * @brief Calculates length(magnitude) of this vector.
+     * @return
+     */
     inline double length() const
     {
-        return std::sqrt(x*x + y*y + z*z);
+        return std::sqrt(x * x + y * y + z * z);
     }
 
+    /**
+     * @brief Checks if two vectors are equivalent.
+     * @param rhs
+     * @return
+     */
     inline bool operator ==(const Vector3& rhs) const
     {   //TODO: Epsilon check
         return (x == rhs.x) && (y == rhs.y) && (z == rhs.z);
     }
 
+    /**
+     * @brief Checks if two vectors are not equivalent.
+     * @param rhs
+     * @return
+     */
     inline bool operator !=(const Vector3& rhs) const
     {
         return !(*this == rhs);
     }
 
+    /**
+     * @brief Subtracts other vector from this and applies result to this vector.
+     * @param rhs
+     * @return Changed this vector.
+     */
     inline Vector3& operator -=(const Vector3& rhs)
     {
         x -= rhs.x;
@@ -72,6 +149,11 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Adds other vector to this and applies result to this vector.
+     * @param rhs
+     * @return Changed this vector.
+     */
     inline Vector3& operator +=(const Vector3& rhs)
     {
         x += rhs.x;
@@ -80,6 +162,11 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Multiplies this vector on double value and applies result to this vector.
+     * @param rhs
+     * @return Changed this vector.
+     */
     inline Vector3& operator *=(const double& rhs)
     {
         x *= rhs;
@@ -88,6 +175,11 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Divides this vector by double value and applies result to this vector.
+     * @param rhs
+     * @return Changed this vector.
+     */
     inline Vector3& operator /=(const double& rhs)
     {
         x /= rhs;
@@ -96,41 +188,91 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Adds other vector to this.
+     * @param lhs
+     * @param rhs
+     * @return New vector = lhs + rhs.
+     */
     friend inline Vector3 operator +(Vector3 lhs, const Vector3& rhs)
     {
         lhs += rhs;
         return lhs;
     }
 
+    /**
+     * @brief Subtracts other vector from this.
+     * @param lhs
+     * @param rhs
+     * @return New vector = lhs - rhs.
+     */
     friend inline Vector3 operator -(Vector3 lhs, const Vector3& rhs)
     {
         lhs -= rhs;
         return lhs;
     }
 
+    /**
+     * @brief Changes sign of all vector values.
+     * @param rhs
+     * @return New vector = -rhs.
+     */
     friend inline Vector3 operator -(Vector3 rhs)
     {
         rhs *= -1;
         return rhs;
     }
 
+    /**
+     * @brief Multiplies this vector on double value.
+     * @param lhs
+     * @param rhs
+     * @return New vector = lhs * rhs
+     */
     friend inline Vector3 operator *(Vector3 lhs, const double& rhs)
     {
         lhs *= rhs;
         return lhs;
     }
 
+    /**
+     * @brief Divides this vector by double value.
+     * @param lhs
+     * @param rhs
+     * @return New vector = lhs / rhs
+     */
     friend inline Vector3 operator /(Vector3 lhs, const double& rhs)
     {
         lhs /= rhs;
         return lhs;
     }
 
+    /**
+     * @brief Calculates distance between this vector and other in three-dimentional space.
+     * @param other
+     * @return
+     */
     inline double distance(const Vector3& other) const
     {
         return ((*this) - other).length();
     }
 
+    /**
+     * @brief Calculates distance between two vectors in three-dimensional space.
+     * @param first
+     * @param second
+     * @return
+     */
+    static inline double distance(const Vector3 &first, const Vector3 &second) const
+    {
+        return first.distance(second);
+    }
+
+    /**
+     *
+     * @param rhl
+     * @return
+     */
     inline Vector3& operator=(const Vector3& rhl)
     {
         x = rhl.x;
@@ -139,22 +281,38 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Normalizes this vector.
+     * @return
+     */
     inline Vector3& normalize()
     {
         (*this) /= this->length();
         return *this;
     }
 
+    /**
+     * @brief Returns normalized version of this vector.
+     * @return
+     */
     inline Vector3 normalized() const
     {
         return (*this) / this->length();
     }
 
+    /**
+     * @brief Represents vector values as std::string.
+     * @return
+     */
     std::string toString() const override;
 
     SERIALIZABLE(double, x, 0);
     SERIALIZABLE(double, y, 0);
     SERIALIZABLE(double, z, 0);
 };
+
+/**
+ * @}
+ */
 
 #endif //DSD_GAMEENGINE_VECTOR3_H
